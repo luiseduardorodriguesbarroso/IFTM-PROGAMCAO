eb66cfe21c73dea9f9192290f1cbc411255efa13
Luís Eduardo Rodrigues Barroso <luiseduardorodriguesbarroso@gmail.com>
2025-07-20 18:38:25 -0300

    calculadora de peso ideal

---
+++ ALGORITMO DE PROGAMAÇÃO/EXERCICIOS/output/output/balaçã.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <windows.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+#include <math.h>
+#include <ctype.h>
+#include <conio.h>
+//==========================
+void limpatl();
+void pouse();
+//==========================
+void limpatl()
+{
+    system("cls");
+}
+//==========================
+void pouse()
+{
+    Sleep(200);
+}
+//==========================
+
+//==========================
+
+//==========================
+
+//==========================
+
+//==========================
+
+//==========================
+int main ()
+{
+
+}
