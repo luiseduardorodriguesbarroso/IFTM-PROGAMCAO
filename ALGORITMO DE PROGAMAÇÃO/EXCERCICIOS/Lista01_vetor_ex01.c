73c879515928ccf43ad76d4c26e337fc9860ff3c
Luís Eduardo Rodrigues Barroso <luiseduardorodriguesbarroso@gmail.com>
2025-08-07 19:09:58 -0300

    Update Lista01_vetor_ex01.c

--- ALGORITMO DE PROGAMAÇÃO/EXCERCICIOS/Lista01_vetor_ex01.c
+++ ALGORITMO DE PROGAMAÇÃO/EXCERCICIOS/Lista01_vetor_ex01.c
@@ -11,6 +11,10 @@
 //===============================================================
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+#include <math.h>
+#include <time.h>
 #include <windows.h>
 #define TAM 10
 
