db0e346f4083a4a3de1117d83843184353526965
Luís Eduardo Rodrigues Barroso <luiseduardorodriguesbarroso@gmail.com>
2025-08-23 21:57:44 -0300

    Add vector utility functions and update date
    Added functions for receiving, displaying, and placeholders for finding the largest and smallest values in a vector. Introduced a constant for vector size and updated the assignment date in the header.

--- ALGORITMO DE PROGAMAÇÃO/EXCERCICIOS/VETOR/LISTA 02/Lista02_vetor_ex01.c
+++ ALGORITMO DE PROGAMAÇÃO/EXCERCICIOS/VETOR/LISTA 02/Lista02_vetor_ex01.c
@@ -4,7 +4,7 @@
     1º PERÍODO - DISCIPLINA DE LINGUAGEM DE PROGRAMAÇÃO 
     1 SEMESTRE 2025
     Prof. Ernani Claudio Borges
-    data: 06/08/2025
+    data: 23/08/2025
 */
 //===============================================================
 //===============================================================
@@ -17,15 +17,17 @@
 #include <math.h>
 #include <time.h>
 #include <windows.h>
+#define tam 6 
 
 //===============================================================
 //============== LOCAL PARA DECLARAR OS PROTOTIPOS ==============
 //===============================================================
 void limpartl ();
-
-
-
-
+void pouse ();
+void receber_vl (int vt[tam]);
+void mostra_vl (int vt[tam]);
+void maior_vt (int vt[tam]);
+void menor_vt (int vt[tam]);
 
 //===============================================================
 //============== LOCAL PARA CRIAR OS MÓDULOS DE FUNÇÕES =========
@@ -35,20 +37,51 @@ void limpartl ()
     system("cls");
 }
 //===============================================================
-
+void pouse ()
+{
+    sleep(200);//Pausa a tela por 0,2 segundos...
+}
 //===============================================================
-
+void receber_vl (int vt[tam])
+{
+    int i=0;
+    limpartl();
+    for (i=0;i<tam;i++)
+    {
+        printf("\nInforme o valor do vetor na posicao [%d]",i);
+        fflush(stdin);
+        scanf("%d",&vt[i]);
+    }    
+}
 //===============================================================
-
+void mostra_vl (int vt[tam])
+{
+    int i=0;
+    limpartl();
+    printf("\nTodos os valores do vetor informadas...\n");
+    for (i=0;i<tam;i++)
+    {
+        print("%6d",vt[i]);
+        pouse();
+    }    
+}
 //===============================================================
-
+void maior_vt (int vt[tam])
+{
+    
+}
+//===============================================================
+void menor_vt (int vt[tam])
+{
+    
+}
 //===============================================================
 //==============  CODIGO PRINCIPAL ==============================
 //===============================================================
 int main()
 {
     //CRIAR VARIAVEIS
-    
+    int vt[tam];
     
     //INICIALIZAR VARIAVEIS
     
