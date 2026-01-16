8b3c5e82ac3d1cc890d95eb8fc2a701c2c3b64a2
Luís Eduardo Rodrigues Barroso <luiseduardorodriguesbarroso@gmail.com>
2026-01-15 21:58:07 -0300

    Refatora lógica de processamento dos vetores R1 e R2
    Corrige e aprimora o processamento dos vetores R1 e R2, separando corretamente os números pares e ímpares de V1. Para pares, soma o valor ao total de V2 e armazena em R1; para ímpares, conta divisores em V2 e armazena em R2. Adiciona exibição clara dos resultados finais e comentários explicativos.

--- ALGORITMO DE PROGAMAÇÃO/EXCERCICIOS/VETOR/LISTA 03/Lista03_vetor_ex01.c
+++ ALGORITMO DE PROGAMAÇÃO/EXCERCICIOS/VETOR/LISTA 03/Lista03_vetor_ex01.c
@@ -4,7 +4,7 @@
     1º PERÍODO - DISCIPLINA DE LINGUAGEM DE PROGRAMAÇÃO 
     1 SEMESTRE 2025
     Prof. Ernani Claudio Borges
-    data: 28/09/2025
+    Data: 15/01/2026
 */
 //===============================================================
 //===============================================================
@@ -22,7 +22,6 @@
 //===============================================================
 void limpartl ();
 
-
 //===============================================================
 //============== LOCAL PARA CRIAR OS MÓDulos DE FUNÇÕES =========
 //===============================================================
@@ -39,51 +38,87 @@ void limpartl ()
 //===============================================================
 
 //===============================================================
-
-//===============================================================
 int main()
 {
     limpartl(); // Limpa a tela para melhorar a amostragem do codigo
 
     //VARIAVEIS 
-    int v1 [10],v2[5];// Meus 2 vetores para receber valores 
-    int i; // Vai ser minha forma de movimentar dendro dos vetores v1 e v2...
-    int R1[10],R2[10]; //R1 e R2 irão ser usados como nossos resultantes...
+    int v1[10], v2[5]; // Meus 2 vetores para receber valores 
+    int i, j; // i e j para ser minha forma de movimentar dendro dos vetores
+    
+    // R1 e R2 precisam ser vetores para guardar varios resultados, nao so um numero
+    int R1[10], R2[10]; 
+    
+    // Variaveis para controlar a posicao dentro de R1 e R2
+    int posR1 = 0; 
+    int posR2 = 0;
 
     //RECEBENDO OS VALORES DOS VETORES 
+    printf("\n--- PREENCHENDO VETOR V1 ---\n");
     for(i=0;i<10;i++)
     {
-        printf("\nInforme o valor do vetor V1 na posicao [%d]...: ",i);
+        printf("Informe o valor do vetor V1 na posicao [%d]...: ",i);
         fflush(stdin);
-        scanf("%d",&v1[i]);
+        scanf("%d",&v1[i]); // Corrigido: precisa do [i] para saber onde guardar
 
     }
+    printf("\n--- PREENCHENDO VETOR V2 ---\n");
     for(i=0;i<5;i++)
     {
-        printf("\nInforme o valor do vetor V2 na posicao [%d]...: ",i);
+        printf("Informe o valor do vetor V2 na posicao [%d]...: ",i);
         fflush(stdin);
-        scanf("%d",&v2[i]);
+        scanf("%d",&v2[i]); // Corrigido: precisa do [i] aqui tambem
 
     }    
-    //Calculando os valores dos resultantes de R1...
-    int soma = 0;//Res vai ser = a soma de tdos os valores do vetor V2 
+    
+    //Calculando a soma de V2 para usar no calculo de R1...
+    int soma = 0; //Res vai ser = a soma de tdos os valores do vetor V2 
     for(i=0;i<5;i++)
     {
         soma = soma + v2[i];
     }
+
+    // PROCESSANDO OS DADOS
+    // Percorrendo o vetor V1 para ver quem e par e quem e impar
     for (i=0;i<10;i++)
     {
+        // Se for PAR vai para o R1
         if(v1[i] % 2 == 0)
         {
-            soma = v1[i] + soma;
-            printf("\n%d",soma);
+            R1[posR1] = v1[i] + soma;
+            posR1++; // Pula para a proxima casa do vetor R1
         }
+        // Se for IMPAR vai para o R2
         else 
         {
-            soma = 0;
-            printf("\n%d",soma);
+            // A regra do R2 e contar quantos divisores tem no V2
+            int contDiv = 0;
+            // Vou percorrer o V2 todo para testar a divisao
+            for(j=0; j<5; j++)
+            {
+                // Verifica se o numero do V1 divide pelo do V2
+                if (v2[j] != 0 && v1[i] % v2[j] == 0)
+                {
+                    contDiv++;
+                }
+            }
+            R2[posR2] = contDiv;
+            posR2++; // Pula para a proxima casa do vetor R2
         }
     }
 
+    // MOSTRANDO OS RESULTADOS FINAIS
+    printf("\n\n================ RESULTADOS ================\n");
     
+    printf("Vetor R1 (Resultante dos Pares): ");
+    for(i=0; i<posR1; i++)
+    {
+        printf("[%d] ", R1[i]);
+    }
+
+    printf("\n\nVetor R2 (Resultante dos Impares): ");
+    for(i=0; i<posR2; i++)
+    {
+        printf("[%d] ", R2[i]);
+    }
 }
