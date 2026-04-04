dbf9a460eba242e67d88e1a172a46158368dc114
luiseduardorodriguesbarroso <luiseduardorodriguesbarroso@gmail.com>
2026-04-04 01:06:00 -0300

    Adicionar estrutura de cadastro e funções para manipulação e exibição de dados pessoais

--- ESTRUTURA DE DADOS/EXERCICIOS/Structs/LIsta teste struct.c
+++ ESTRUTURA DE DADOS/EXERCICIOS/Structs/LIsta teste struct.c
@@ -33,7 +33,15 @@
 //===============================================================
 //==============              STRUCTS              ==============
 //===============================================================
+typedef struct cadastro
+{
+    char nome[255];
+    int idade;
+    char sexo[50];
+    char cpf[15];
+    float altura;
 
+} cadastro;
 //===============================================================
 
 //===============================================================
@@ -48,8 +56,8 @@
 //==================== PROTOTIPOS ===============================
 //===============================================================
 void limpa_tl();
-
-
+void cadastroluiz ();
+void mostarcadastro (char nome[255],int idade,char cpf[15],char sexo[50], float altura);
 
 
 //===============================================================
@@ -64,9 +72,30 @@ void limpa_tl()
     #endif
 }
 //===============================================================
+void cadastroluiz ()
+{
+    cadastro luiz; //Cadastro teste do Luiz
+    luiz.idade = 19;
+    luiz.altura = 1.85;
+    strcpy(luiz.cpf,"123.456.789-10");
+    strcpy(luiz.sexo,"Masculino");
+    strcpy(luiz.nome, "Luis Henrique Leao Camargo");
 
+    cadastro camila; //Cadastro teste da Camila
+    camila.idade = 29;
+    camila.altura = 1.65;
+    strcpy(camila.cpf,"735.456.000-37");
+    strcpy(camila.sexo, "Femenino");
+    strcpy(camila.nome,"Camila Alves Jardin Santos");
+
+    mostarcadastro(luiz.nome,luiz.idade,luiz.sexo,luiz.cpf,luiz.altura); //Mostra o cadastro do Luiz
+    mostarcadastro(camila.nome,camila.idade,camila.sexo,camila.cpf,camila.altura); //Mostra o cadastro da Camila 
+}
 //===============================================================
-
+void mostarcadastro (char nome[255],int idade,char cpf[15],char sexo[50], float altura)
+{
+    printf("\nNome:%s \nIdade:%d \nSexo:%s \nCPF:%s \nAltura:%.2fcm\n",nome,idade,sexo,cpf,altura);
+}
 //===============================================================
 
 //===============================================================
@@ -87,8 +116,7 @@ void limpa_tl()
 int main()
 {
     limpa_tl(); //Limpa a tela para uma melhor amostragem 
-
-
+    cadastroluiz(); 
 
     //FINALIZAR PROGRAMA
     printf("\n\n\n Acabou ... Tchau ... obrigado ... \n\n\n");
