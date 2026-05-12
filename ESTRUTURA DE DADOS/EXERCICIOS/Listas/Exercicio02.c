6dd47679e41be284e6b590176e5fef7ba106b1c2
luiseduardorodriguesbarroso <luiseduardorodriguesbarroso@gmail.com>
2026-05-11 22:52:36 -0300

    Adicionar arquivo Exercicio02.c com implementação da estrutura de lista e função para criar elementos

---
+++ ESTRUTURA DE DADOS/EXERCICIOS/Listas/Exercicio02.c
@@ -0,0 +1,48 @@
+/*===============================================================
+
+    CURSO: ENGENHARIA DE COMPUTAÇÃO
+    Nome: Luís Eduardo Rodrigues Barroso
+    3º PERÍODO - DISCIPLINA DE LINGUAGEM DE PROGRAMAÇÃO 
+    1 SEMESTRE 2026
+    Prof. Rafael Orbolato
+    Data: 11/05/2026
+*/
+//===============================================================
+//===============================================================
+//======== LOCAL PARA DECLARAR AS INCLUDES E VARS GLOBAIS =======
+//===============================================================
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <math.h>
+#include <time.h>
+#include <unistd.h>
+#include <stdbool.h>
+#include <limits.h>
+#include <float.h>
+#include <stdint.h>
+#include <assert.h>
+#include <windows.h>
+
+typedef struct numero 
+{
+    int numero;
+    struct numero *proximo;
+}num;
+
+num* criarelemento ()
+{
+    num* numerocriado = malloc(sizeof(num));
+    return numerocriado;
+}
+
+int main()
+{
+    num* comecodalista = criarelemento();
+    comecodalista->proximo = criarelemento();
+   
+    //FINALIZAR PROGRAMA
+    printf("\n\n\n Acabou ... Tchau ... obrigado ... \n\n\n");
+    return 0;
+}
