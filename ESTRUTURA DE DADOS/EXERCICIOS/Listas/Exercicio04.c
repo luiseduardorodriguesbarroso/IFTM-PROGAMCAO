5529c99d7e586d89d960545838224c1b9f3baaca
Luís Eduardo Rodrigues Barroso <luiseduardorodriguesbarroso@gmail.com>
2026-05-13 09:37:13 -0300

    Adicionar arquivo Exercicio04.c com implementação da função main e cabeçalho

---
+++ ESTRUTURA DE DADOS/EXERCICIOS/Listas/Exercicio04.c
@@ -0,0 +1,32 @@
+/*===============================================================
+
+    CURSO: ENGENHARIA DE COMPUTAÇÃO
+    Nome: Luís Eduardo Rodrigues Barroso
+    3º PERÍODO - DISCIPLINA DE LINGUAGEM DE PROGRAMAÇÃO 
+    1 SEMESTRE 2026
+    Prof. Rafael Orbolato
+    Data: 11/05/2026
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <math.h>
+#include <time.h>
+#include <unistd.h>
+#include <stdbool.h>
+#include <limits.h>
+#include <float.h>
+#include <stdint.h>
+#include <assert.h>
+
+int main()
+{
+
+    
+    
+
+    //FINALIZAR PROGRAMA
+    printf("\n\n\n Acabou ... Tchau ... obrigado ... \n\n\n");
+    return 0;
+}
