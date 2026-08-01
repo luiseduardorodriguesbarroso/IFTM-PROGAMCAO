cb3504274b0a705963a1be5c328b3b615e8e1994
luiseduardorodriguesbarroso <luiseduardorodriguesbarroso@gmail.com>
2026-07-31 22:54:23 -0300

    Adicionar arquivo Exrcicio07.c com função para exibir o dia da semana por extenso

---
+++ ESTRUTURA DE DADOS/EXERCICIOS/Funções/Exrcicio07.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <math.h>
+#include <time.h>
+#include <unistd.h>
+#include <stdbool.h>
+#include <limits.h>
+#include <float.h>
+#include <stdint.h>
+#include <assert.h>
+
+//Crie uma função que recebe como parâmetros um dia, um mês e um ano (inteiros) e escreva na tela por extenso o dia da semana.
+
+
+int main() {
+    
+    return 0;
+}
