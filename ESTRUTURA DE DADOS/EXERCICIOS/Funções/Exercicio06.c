c6c43229ed891b3fa9933efe894234c43bb9650e
luiseduardorodriguesbarroso <luiseduardorodriguesbarroso@gmail.com>
2026-07-31 22:52:15 -0300

    Adicionar arquivo Exercicio06.c com função para calcular o dia da semana

---
+++ ESTRUTURA DE DADOS/EXERCICIOS/Funções/Exercicio06.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <math.h>
+#include <time.h>
+#include <unistd.h>
+#include <stdbool.h>
+#include <limits.h>
+#include <float.h>
+#include <stdint.h>
+#include <assert.h>
+
+/*
+Escreva uma função que receba dia, mês e ano e calcule o dia da semana. Esta função retorna 0 se for domingo, 1 se for segunda-feira, etc... Para calcular o dia da semana use o código a seguir no qual o dia da semana está na variável f:
+int f = ano + dia + 3 * (mês - 1) - 1;
+if (mês < 3) ano--;
+else f -= (int)(0.4 * mês + 2.3);
+f += (int)(ano/4) - (int)((ano/100 + 1) * 0.75);
+f %= 7;
+*/
+
+
+int main() {
+    
+    return 0;
+}
