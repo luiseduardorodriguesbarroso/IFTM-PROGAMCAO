602be674ff220a97d2aee78f435591c8b6a9c07d
Luís Eduardo Rodrigues Barroso <luiseduardorodriguesbarroso@gmail.com>
2026-08-05 10:56:56 -0300

    Adicionar função para preencher vetor com números aleatórios

--- ESTRUTURA DE DADOS/EXERCICIOS/Funções/Lista 02/Exercicio02.c
+++ ESTRUTURA DE DADOS/EXERCICIOS/Funções/Lista 02/Exercicio02.c
@@ -12,9 +12,24 @@
 #include <assert.h>
 
 //Crie uma função em C que recebe como parâmetros um vetor de inteiros, seu tamanho, um valor máximo e um valor mínimo. A função deve preencher o vetor com números aleatórios dentro dessa faixa de valores.
+void preencherVetor(int vetor[], int tamanho, int min, int max);
 
-void preenchervetor(int max, int min);
 int main() {
+    int vt[10];
+    srand(time(NULL));
+
+    preencherVetor(vt,10,3,50);
     
     return 0;
+}
+
+void preencherVetor(int vetor[], int tamanho, int min, int max) {
+    for (int i = 0; i < tamanho; i++) {
+        vetor[i] = rand() % (max - min + 1) + min;
+    }
+    printf("Vetor gerado:\n");
+    for (int i = 0; i < 10; i++) {
+        printf("%d ", vetor[i]);
+    }
+    printf("\n");
 }
