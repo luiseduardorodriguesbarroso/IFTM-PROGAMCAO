1143cc18574325650aaf58dbb71fba7fd8928368
Luís Eduardo Rodrigues Barroso <luiseduardorodriguesbarroso@gmail.com>
2026-08-05 10:29:16 -0300

    Adicionar Exercicio02.c com função para preencher vetor com números aleatórios

---
+++ ESTRUTURA DE DADOS/EXERCICIOS/Funções/Lista 02/Exercicio02.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <math.h>
+#include <time.h>
+#include <unistd.h>
+#include <stdbool.h>
+#include <limits.h>
+#include <float.h>
+#include <stdint.h>
+#include <assert.h>
+
+//Crie uma função em C que recebe como parâmetros um vetor de inteiros, seu tamanho, um valor máximo e um valor mínimo. A função deve preencher o vetor com números aleatórios dentro dessa faixa de valores.
+
+void preenchervetor(int max, int min);
+int main() {
+    
+    return 0;
+}
