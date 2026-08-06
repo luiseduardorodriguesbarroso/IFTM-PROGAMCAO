c0712b81a707faaf4da747c1aff8d3e8a3658127
Luís Eduardo Rodrigues Barroso <luiseduardorodriguesbarroso@gmail.com>
2026-08-06 11:36:49 -0300

    Adicionar função para cadastrar restaurante e ler dados do usuário

--- ESTRUTURA DE DADOS/EXERCICIOS/Structs/Lista 01/Exercicio01.c
+++ ESTRUTURA DE DADOS/EXERCICIOS/Structs/Lista 01/Exercicio01.c
@@ -16,14 +16,37 @@ typedef struct{
     char endereço[250];
     float preçomedio;
     char tipoComida[250];
-
-
+    
 } restalrante;
 
+restalrante cadastrarrestalrante ();
+void lerString(char texto[], int tamanho);
+
 int main() {
+    restalrante  r = cadastrarrestalrante();
+
     
 
-
-    
     return 0;
+}
+
+restalrante cadastrarrestalrante (){
+    restalrante piriquito;    
+
+    printf("\nInforme o nome do restalrante: ");
+    lerString(piriquito.nome, 20);
+    printf("\nInforme endereço: ");
+    lerString(piriquito.endereço, 250);
+    printf("\nInforme o preço medio de cada prato: ");
+    scanf("%d",&piriquito.preçomedio);
+    printf("\nInforme tipo de comida: ");
+    lerString(piriquito.tipoComida, 250);
+
+
+    return piriquito;
+}
+
+void lerString(char texto[], int tamanho) {
+    fgets(texto, tamanho, stdin);
+    texto[strcspn(texto, "\n")] = '\0';
 }
