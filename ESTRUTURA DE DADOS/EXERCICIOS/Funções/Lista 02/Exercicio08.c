20912f79d60c9b30dff41ae96773286c7224d890
luiseduardorodriguesbarroso <luiseduardorodriguesbarroso@gmail.com>
2026-08-05 23:57:20 -0300

    Adicionar arquivos Exercicio08.c, Exercicio09.c e Exercicio01.c com estrutura inicial e comentários para implementação de funções

---
+++ ESTRUTURA DE DADOS/EXERCICIOS/Funções/Lista 02/Exercicio08.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <math.h>
+#include <time.h>
+#include <unistd.h>
+#include <stdbool.h>
+#include <limits.h>
+#include <float.h>
+#include <stdint.h>
+#include <assert.h>
+
+//Crie uma função em C que recebe como parâmetros um vetor de inteiros e seu tamanho. A função deve retornar 1 caso os elementos estejam em ordem crescente, 2 caso os elementos estejam em ordem decrescente e 0 caso estejam fora de ordem.
+
+
+int main() {
+    
+    return 0;
+}
---
+++ ESTRUTURA DE DADOS/EXERCICIOS/Funções/Lista 02/Exercicio09.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <math.h>
+#include <time.h>
+#include <unistd.h>
+#include <stdbool.h>
+#include <limits.h>
+#include <float.h>
+#include <stdint.h>
+#include <assert.h>
+
+/*No programa principal:
+
+a) Criar um vetor de tamanho 20 de inteiros
+b) Preencher os 10 primeiros valores do vetor com números digitados pelo usuário via teclado
+c) Preencher os 10 últimos valores do vetor com números aleatórios entre 1 e 100
+d) Mostrar o vetor inteiro na tela
+e) Calcular e mostrar a média dos valores do vetor
+f) Encontrar e mostrar o maior e o menor valor do vetor
+g)Verificar se há elementos repetidos no vetor e exibir uma mensagem em caso afirmativo
+h) Solicitar um valor ao usuário e verificar se esse valor existe no vetor. Se existir exibir a posição em que ele está, senão exibir uma mensagem de valor não encontrado
+i) Verificar se o vetor está em ordem crescente, ou seja, se a[0] <= a[1] <= a[2] <= ... para todos elementos do vetor
+
+Use as funções que você criou!
+*/
+
+
+int main() {
+    
+    return 0;
+}
