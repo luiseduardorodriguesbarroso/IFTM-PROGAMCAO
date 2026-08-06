d57f59a121d9b7452f1e083bc62c6e2412e57946
Luís Eduardo Rodrigues Barroso <luiseduardorodriguesbarroso@gmail.com>
2026-08-06 11:53:09 -0300

    Adicionar função para mostrar índice de valor em vetor e exibir resultados

--- ESTRUTURA DE DADOS/EXERCICIOS/Funções/Lista 02/Exercicio07.c
+++ ESTRUTURA DE DADOS/EXERCICIOS/Funções/Lista 02/Exercicio07.c
@@ -11,12 +11,49 @@
 #include <stdint.h>
 #include <assert.h>
 
-//) Crie uma função em C que recebe como parâmetros um vetor de inteiros, seu tamanho e um valor inteiro. A função deve retornar o índice do elemento do vetor com esse valor ou -1 caso ele não exista no vetor.
-
+//Crie uma função em C que recebe como parâmetros um vetor de inteiros, seu tamanho e um valor inteiro. A função deve retornar o índice do elemento do vetor com esse valor ou -1 caso ele não exista no vetor.
+int mostrarindice(int vetor[], int tamanho, int min, int max);
 
 int main() {
     int vt[5];
+    int indice;
+
     srand(time(NULL));
-    
+
+    indice = mostrarindice(vt, 5, 2, 20);
+
+    if (indice == -1) {
+        printf("\nValor nao encontrado.\n");
+    } else {
+        printf("\nValor encontrado na posicao %d.\n", indice);
+    }
     return 0;
+}
+
+int mostrarindice(int vetor[], int tamanho, int min, int max) {
+    int valor;
+
+    for (int i = 0; i < tamanho; i++) {
+        vetor[i] = rand() % (max - min + 1) + min;
+    }
+
+    printf("\nIndice:");
+    for (int i = 0; i < tamanho; i++) {
+        printf("%5d", i);
+    }
+
+    printf("\nVetor :");
+    for (int i = 0; i < tamanho; i++) {
+        printf("%5d", vetor[i]);
+    }
+
+    printf("\n\nInforme o valor que deseja procurar: ");
+    scanf("%d", &valor);
+
+    for (int i = 0; i < tamanho; i++) {
+        if (vetor[i] == valor) {
+            return i;
+        }
+    }
+    return -1;
 }
