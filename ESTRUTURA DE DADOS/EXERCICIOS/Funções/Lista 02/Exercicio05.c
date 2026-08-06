c5c56a27d08fb5006641dee1f3a9f26fd02273da
luiseduardorodriguesbarroso <luiseduardorodriguesbarroso@gmail.com>
2026-08-05 21:54:31 -0300

    Adicionar funções para encontrar o menor valor e verificar elementos repetidos em um vetor

--- ESTRUTURA DE DADOS/EXERCICIOS/Funções/Lista 02/Exercicio05.c
+++ ESTRUTURA DE DADOS/EXERCICIOS/Funções/Lista 02/Exercicio05.c
@@ -11,17 +11,17 @@
 #include <stdint.h>
 #include <assert.h>
 
-//Crie uma função em C que recebe como parâmetros um vetor de inteiros e seu tamanho. A função deve encontrar e retornar o índice do maior valor do vetor.
-void procurarmaior(int vetor[],int tamanho, int min, int max);
+//) Crie uma função em C que recebe como parâmetros um vetor de inteiros e seu tamanho. A função deve encontrar e retornar o índice do menor valor do vetor.
+void procurarmenor(int vetor[],int tamanho, int min, int max);
 
 int main() {
-    int vt [5]; 
+    int vt [5];
     srand(time(NULL));
-    procurarmaior(vt,5,2,20);
+    procurarmenor(vt,5,2,20);
     return 0;
 }
 
-void procurarmaior(int vetor[],int tamanho, int min, int max){
+void procurarmenor(int vetor[],int tamanho, int min, int max){
     for (int i = 0; i < tamanho; i++) {
         vetor[i] = rand() % (max - min + 1) + min;
     }
@@ -31,17 +31,17 @@ void procurarmaior(int vetor[],int tamanho, int min, int max){
     }
     printf("\n");
 
-    int maior;
+    int menor;
     int inicio=0;
     for (int i = 1; i < tamanho; i++)
     {
-        if (vetor[inicio] > vetor[i])
+        if (vetor[inicio] < vetor[i])
         {
-            maior = vetor[inicio];
+            menor = vetor[inicio];
         } else {
-            maior = vetor[i];
+            menor = vetor[i];
             inicio++;
         }
     }
-    printf("Maior numero desse vetor é: %d\n",maior);
+    printf("Menor numero desse vetor é: %d\n",menor);
 }
---
+++ ESTRUTURA DE DADOS/EXERCICIOS/Funções/Lista 02/Exercicio06.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <math.h>
+#include <time.h>
+#include <unistd.h>
+#include <stdbool.h>
+#include <limits.h>
+#include <float.h>
+#include <stdint.h>
+#include <assert.h>
+
+//) Crie uma função em C que recebe como parâmetros um vetor de inteiros e seu tamanho. A função deve retornar 1 caso o vetor tenha elementos repetidos e 0 caso contrário.
+int procurar_repetidos(int vetor[],int tamanho, int min, int max);
+
+int main() {
+    int vt [5];
+    srand(time(NULL));
+    procurar_repetidos(vt,5,2,10);
+    return 0;
+}
+
+int procurar_repetidos(int vetor[],int tamanho, int min, int max){
+    for (int i = 0; i < tamanho; i++) {
+        vetor[i] = rand() % (max - min + 1) + min;
+    }
+    printf("Vetor gerado:\n");
+    for (int i = 0; i < tamanho; i++) {
+        printf("%d ", vetor[i]);
+    }
+    printf("\n");
+
+    for (int i = 0; i < tamanho - 1; i++) {
+
+        for (int j = i + 1; j < tamanho; j++) {
+
+            if (vetor[i] == vetor[j]) {
+                printf("O numero %d esta repetido.\n", vetor[i]);
+                return 1;
+            }
+        }
+    }
+    return 0;
+    
+}
