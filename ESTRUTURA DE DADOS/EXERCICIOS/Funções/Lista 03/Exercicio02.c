0fbb9214297f5f6be322cce04e421cf9be72c2e3
luiseduardorodriguesbarroso <luiseduardorodriguesbarroso@gmail.com>
2026-08-07 20:59:08 -0300

    Adicionar função para preencher e exibir matriz 3x3 com valores aleatórios; criar novos arquivos para exercícios 03 a 09

--- ESTRUTURA DE DADOS/EXERCICIOS/Funções/Lista 03/Exercicio02.c
+++ ESTRUTURA DE DADOS/EXERCICIOS/Funções/Lista 03/Exercicio02.c
@@ -16,9 +16,30 @@
 	D	E	F
 	G	H	I
  */
-
+void mostrarmatrizabcd(int matriz[3][3], int min, int max);
 
 int main() {
-    
+    int mt[3][3];
+	srand(time(NULL));
+	mostrarmatrizabcd(mt,2,20);
     return 0;
+}
+
+void mostrarmatrizabcd(int matriz[3][3], int min, int max){
+	for (int i = 0; i < 3; i++)
+	{
+		for (int j = 0; j < 3; j++)
+		{
+			 matriz[i][j] = rand() % (max - min + 1) + min;
+		}
+	}
+	for (int i = 0; i < 3; i++)
+	{
+		for (int j = 0; j < 3; j++)
+		{
+			printf("%5d",matriz[i][j]);
+		}
+		printf("\n");
+	}
+	
 }
---
+++ ESTRUTURA DE DADOS/EXERCICIOS/Funções/Lista 03/Exercicio07.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <math.h>
+#include <time.h>
+#include <unistd.h>
+#include <stdbool.h>
+#include <limits.h>
+#include <float.h>
+#include <stdint.h>
+#include <assert.h>
+
+int main() {
+    
+    return 0;
+}
