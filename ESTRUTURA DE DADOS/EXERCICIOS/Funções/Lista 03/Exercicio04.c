b09c6d6f6b8de783fabf34a0eedf7fc44f232b6a
luiseduardorodriguesbarroso <luiseduardorodriguesbarroso@gmail.com>
2026-08-09 15:00:26 -0300

    Adicionar função para gerar matriz 3x3 aleatória e encontrar o maior valor

--- ESTRUTURA DE DADOS/EXERCICIOS/Funções/Lista 03/Exercicio04.c
+++ ESTRUTURA DE DADOS/EXERCICIOS/Funções/Lista 03/Exercicio04.c
@@ -12,9 +12,39 @@
 #include <assert.h>
 
 //Crie uma função que receba como parâmetro uma matriz 3x3 de inteiros. A função deve encontrar e retornar o maior valor da matriz.
-
+void procurar_maior(int mt[3][3], int min, int max);
 
 int main() {
-    
+    int mt [3][3];
+    srand(time(NULL));
+    procurar_maior(mt,2,20);
     return 0;
+}
+
+void procurar_maior(int mt[3][3], int min, int max){
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            mt[i][j] = rand() % (max - min + 1) + min;
+        }
+    }
+    int maior;
+     maior = mt[0][0];
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            if(mt[i][j]>maior){
+                maior = mt[i][j];
+            }
+        }  
+    }
+    printf("Matriz gerada:\n");
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            printf("%5d",mt[i][j]);
+        }
+        printf("\n");
+    }
+    printf("\nO maior valor dessa matriz e : %d",maior);
+
 }
