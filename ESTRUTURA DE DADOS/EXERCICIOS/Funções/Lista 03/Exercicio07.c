54f98d3cd4538b3b90fbf023fa4536bfa12d676a
luiseduardorodriguesbarroso <luiseduardorodriguesbarroso@gmail.com>
2026-08-10 00:34:02 -0300

    Adicionar função para somar duas matrizes 3x3 e exibir o resultado

--- ESTRUTURA DE DADOS/EXERCICIOS/Funções/Lista 03/Exercicio07.c
+++ ESTRUTURA DE DADOS/EXERCICIOS/Funções/Lista 03/Exercicio07.c
@@ -12,9 +12,55 @@
 #include <assert.h>
 
 //Crie uma função que receba como parâmetros três matrizes 3x3 de inteiros. A função deve somar a 1a e a 2a matrizes na 3a.
-
+void somar_matrizes(int mt[3][3],int mt2[3][3],int mt3[3][3],int min, int max);
 
 int main() {
     
     return 0;
+}
+
+void somar_matrizes(int mt[3][3],int mt2[3][3],int mt3[3][3],int min, int max){
+    for (int i = 0; i < 3; i++) { //Gerando mt1
+        for (int j = 0; j < 3; j++) {
+            mt[i][j] = rand() % (max - min + 1) + min;
+        }
+    }//Gerando mt2
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            mt2[i][j] = rand() % (max - min + 1) + min;
+        }
+    }//Mostrando mt1
+    printf("Matrriz ");
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            printf("%5d",mt[i][j]);
+        }
+        printf("\n");
+    }//Mostrando mt2
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            printf("%5d",mt2[i][j]);
+        }
+        printf("\n");
+    }
+    //Calculando mt3
+    mt3 [0][0] = mt[0][0] + mt2[0][0];
+    mt3 [0][1] = mt[0][1] + mt2[0][1];
+    mt3 [0][2] = mt[0][2] + mt2[0][2];
+
+    mt3 [1][0] = mt[1][0] + mt2[1][0];
+    mt3 [1][1] = mt[1][1] + mt2[1][1];
+    mt3 [1][2] = mt[1][2] + mt2[1][2];
+
+    mt3 [2][0] = mt[2][0] + mt2[2][0];
+    mt3 [2][1] = mt[2][1] + mt2[2][1];
+    mt3 [2][2] = mt[2][2] + mt2[2][2];
+
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            printf("%5d",mt3[i][j]);
+        }
+        printf("\n");
+    }
+
 }
