438413499bf9938924853d2883324061f68543d4
luiseduardorodriguesbarroso <luiseduardorodriguesbarroso@gmail.com>
2026-08-24 23:03:37 -0300

    Adicionar função para exibir elementos de vetor e criar novo arquivo para preenchimento de vetor com números aleatórios

--- ESTRUTURA DE DADOS/EXERCICIOS/Ponteiros/Lista 02/Exercicio02.c
+++ ESTRUTURA DE DADOS/EXERCICIOS/Ponteiros/Lista 02/Exercicio02.c
@@ -13,8 +13,20 @@
 
 //2) Crie uma função em C que recebe como parâmetros um vetor de inteiros via ponteiro e seu tamanho. A função deve mostrar os elementos do vetor na tela. Use aritmética de ponteiros sem alterar o ponteiro.
 
+void elementos_vet(int *vet[],int tamanho);
+
 
 int main() {
-    
+    int vet [5] = {1,2,3,4,5};
+
+    elementos_vet(vet,5);
+
     return 0;
+}
+
+void elementos_vet(int *vet[],int tamanho){
+   
+    for(int i  = 0;i < tamanho;i++){
+        printf("%5d",*(vet + i));
+    }
 }
