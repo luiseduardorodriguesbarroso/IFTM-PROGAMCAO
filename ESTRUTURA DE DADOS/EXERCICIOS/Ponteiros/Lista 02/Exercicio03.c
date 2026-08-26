17d13af6aeadca1058b2133af84d8449dc049424
Luís Eduardo Rodrigues Barroso <luiseduardorodriguesbarroso@gmail.com>
2026-08-26 11:18:06 -0300

    Adicionar função para preencher vetor com números aleatórios e criar novo arquivo para cálculo da média

--- ESTRUTURA DE DADOS/EXERCICIOS/Ponteiros/Lista 02/Exercicio03.c
+++ ESTRUTURA DE DADOS/EXERCICIOS/Ponteiros/Lista 02/Exercicio03.c
@@ -13,8 +13,27 @@
 
 //3) Crie uma função em C que recebe como parâmetros um vetor de inteiros via ponteiro, seu tamanho, um valor máximo e um valor mínimo. A função deve preencher o vetor com números aleatórios dentro dessa faixa de valores. Use o operador de índice no ponteiro.
 
+void preencher_vet(int *vet, int tamanho, int min, int max);
+
+
 
 int main() {
     
+    int vet[10];
+    srand(time(NULL));
+
+    preencher_vet(vet,10,3,20);
+
     return 0;
+}
+
+void preencher_vet(int *vet, int tamanho, int min, int max){
+    for (int i = 0; i < tamanho; i++) {
+        vet[i] = rand() % (max - min + 1) + min;
+    }
+    for (int i = 0; i < tamanho; i++)
+    {
+        printf("%5d",vet[i]);
+    }
+    
 }
---
+++ ESTRUTURA DE DADOS/EXERCICIOS/Ponteiros/Lista 02/Exercicio04.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <math.h>
+#include <time.h>
+#include <unistd.h>
+#include <stdbool.h>
+#include <limits.h>
+#include <float.h>
+#include <stdint.h>
+#include <assert.h>
+
+//4) Crie uma função em C que recebe como parâmetros um vetor de inteiros via ponteiro e seu tamanho. A função deve calcular e retornar a média dos valores do vetor. Use aritmética de ponteiros alterando o ponteiro.
+
+
+
+int main() {
+    
+    return 0;
+}
