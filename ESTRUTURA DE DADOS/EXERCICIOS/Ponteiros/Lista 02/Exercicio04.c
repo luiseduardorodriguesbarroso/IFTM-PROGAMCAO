854161c1ef5542d0038fa3de7e584ec3af6155a4
Luís Eduardo Rodrigues Barroso <luiseduardorodriguesbarroso@gmail.com>
2026-08-26 11:32:06 -0300

    Adicionar função para calcular a média de um vetor de inteiros utilizando aritmética de ponteiros

--- ESTRUTURA DE DADOS/EXERCICIOS/Ponteiros/Lista 02/Exercicio04.c
+++ ESTRUTURA DE DADOS/EXERCICIOS/Ponteiros/Lista 02/Exercicio04.c
@@ -13,9 +13,26 @@
 
 //4) Crie uma função em C que recebe como parâmetros um vetor de inteiros via ponteiro e seu tamanho. A função deve calcular e retornar a média dos valores do vetor. Use aritmética de ponteiros alterando o ponteiro.
 
-
+float media_vet(int *vet,int tamanho);
 
 int main() {
     
+    int vet[10] = {1,2,3,4,5,6,7,8,9,10};
+
+    srand(time(NULL));
+
+    printf("Media do Vt: %.2f",media_vet(vet,10));
     return 0;
+}
+
+float media_vet(int *vet,int tamanho){
+    
+    float somatotal = 0;
+
+    for (int i = 0; i < tamanho; i++)
+    {
+        somatotal = somatotal + vet[i];
+    }
+
+    return somatotal / tamanho;
 }
