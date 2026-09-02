bdbf98c6fc3631f278f2b6ec9d27b69f57c59d5e
Luís Eduardo Rodrigues Barroso <luiseduardorodriguesbarroso@gmail.com>
2026-09-02 11:25:10 -0300

    Adicionar arquivo Exercicio al01.c com estrutura de lista e função de inserção

---
+++ ESTRUTURA DE DADOS/EXERCICIOS/Listas/Exercicio al01.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <math.h>
+#include <time.h>
+#include <unistd.h>
+#include <stdbool.h>
+#include <limits.h>
+#include <float.h>
+#include <stdint.h>
+#include <assert.h>
+
+typedef struct 
+{
+    
+}list;
+
+
+int main() {
+    
+    return 0;
+}
+
+void inserir_ist(int valor,list * pontlist){
+
+
+}
