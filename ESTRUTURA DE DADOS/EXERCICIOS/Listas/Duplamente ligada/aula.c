d9e65d57cb7d7f61383caf213c846e849248f61a
Luís Eduardo Rodrigues Barroso <luiseduardorodriguesbarroso@gmail.com>
2026-09-23 11:37:08 -0300

    Adicionar funções para manipulação de lista duplamente ligada em aula.c

--- ESTRUTURA DE DADOS/EXERCICIOS/Listas/Duplamente ligada/aula.c
+++ ESTRUTURA DE DADOS/EXERCICIOS/Listas/Duplamente ligada/aula.c
@@ -30,9 +30,11 @@ typedef struct listadupla_int{
 //---------------------------------------------------------------
 NoDSLI *criarNoDLI(int valor, NoDSLI *anterior, NoDSLI *proximo);
 listaDLI *criarliataDLI();
-int InserirInicioListaDLI();
-
-
+void mostrarListaDLI(listaDLI *pontlista);
+void inserirInicioListaDLI(int valor, listaDLI *pontlista);
+void inserirFimListaDLS(int valor, listaDLI *pontlista);
+void limparLDLI(listaDLI *pontlista);
+void MostrarListaInvertida(listaDLI *pontlista);
 
 
 
@@ -69,12 +71,104 @@ listaDLI *criarliataDLI(){
     return nova;
 }
 
-int InserirInicioListaDLI(){
-    
+void mostrarListaDLI(listaDLI *pontlista){
+    if (pontlista == NULL){ //se a lista for Excluida ou NULL ela avisa
+        printf("\nLista Vazia\n");
+    }
+
+    printf("Tamanho da Lista = %d\n", pontlista->tamanho);
+
+    if (pontlista->tamanho == 0) {
+        printf("Lista Vazia\n");
+    } else {
+        NoDSLI *pontAux = pontlista->inicio;
+        while (pontAux != NULL) {
+            printf("%d -> ", pontAux->valor);
+            pontAux = pontAux->proximo; // O ponteiro anda para a próxima "gaveta"
+        }
+        printf("NULL\n");
+    } 
+    printf("\n");
 }
 
+void inserirInicioListaDLI(int valor, listaDLI *pontlista){
+
+    NoDSLI *novo  = criarNoDLI(valor,NULL,pontlista->inicio);
+
+    if(pontlista->inicio == NULL) {
+        pontlista->inicio = novo;
+        pontlista->fim = novo; 
+    } else {
+        pontlista->inicio->anterior = novo;
+        pontlista->inicio = novo;
+    }
+    pontlista->tamanho++;
+}
+
+void inserirFimListaDLS(int valor, listaDLI *pontlista){
+    if (pontlista == NULL){
+        printf("Aviso: lista vazia!\n");
+    }
+    NoDSLI *novo = criarNoDLI(valor,pontlista->fim,NULL);
+
+    if (pontlista->inicio == NULL) {  //se inicio for null
+        pontlista->inicio = novo;
+        pontlista->fim = novo;
+    } else {
+        pontlista->fim->proximo = novo;
+        pontlista->fim = novo;
+    }
+
+    pontlista->tamanho++;
+}
+
+void limparLDLI(listaDLI *pontlista){
+    if(pontlista->tamanho == 0)
+    {
+        printf("\nLista já esta vazia\n");
+    } else if (pontlista->tamanho == 1) {
+        free(pontlista->inicio);
+        pontlista->inicio = NULL;
+    } else {
+        NoDSLI *aux = pontlista->inicio;
+        
+        while(aux != NULL){
+            NoDSLI *proximo = aux->proximo;
+            free(aux);
+            aux = proximo;
+        }
+    }
+    pontlista->fim = NULL;
+    pontlista->inicio = NULL;
+    pontlista->tamanho = 0;
+}
+
+void MostrarListaInvertida(listaDLI *pontlista) {
+
+}
 
 int main() {
     
+    listaDLI *lista = criarliataDLI();
+
+    system("clear"); //limpatl linux
+
+    inserirInicioListaDLI(15,lista);
+    mostrarListaDLI(lista);
+
+    inserirInicioListaDLI(50,lista);
+    mostrarListaDLI(lista);
+
+    inserirInicioListaDLI(77,lista);
+    mostrarListaDLI(lista);
+
+    inserirFimListaDLS(19,lista);
+    mostrarListaDLI(lista);
+    
+    inserirFimListaDLS(43,lista);
+    mostrarListaDLI(lista);
+
+    limparLDLI(lista); //Apagando a lista
+    mostrarListaDLI(lista);
     return 0;
 }
