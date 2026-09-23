71f3937f6b70e73ac0314b6950765026ee13d7f6
Luís Eduardo Rodrigues Barroso <luiseduardorodriguesbarroso@gmail.com>
2026-09-23 09:26:59 -0300

    Adicionar implementação da lista duplamente ligada em aula.c

---
+++ ESTRUTURA DE DADOS/EXERCICIOS/Listas/Duplamente ligada/aula.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <math.h>
+#include <time.h>
+#include <unistd.h>
+#include <stdbool.h>
+#include <limits.h>
+#include <float.h>
+#include <stdint.h>
+#include <assert.h>
+
+
+typedef struct nodupla
+{
+    int valor;
+    struct nodupla *anterior;
+    struct nodupla *proximo;
+}NoDSLI;
+
+typedef struct listadupla_int{
+
+    NoDSLI *inicio;
+    NoDSLI *fim;
+    int tamanho;
+
+}listaDLI;
+
+//---------------------------------------------------------------
+NoDSLI *criarNoDLI(int valor, NoDSLI *anterior, NoDSLI *proximo);
+listaDLI *criarliataDLI();
+
+
+
+
+
+
+
+
+//---------------------------------------------------------------
+
+NoDSLI *criarNoDLI(int valor, NoDSLI *anterior, NoDSLI *proximo){
+    NoDSLI * novo = (NoDSLI*) malloc(sizeof(NoDSLI));
+
+    if (novo == NULL){
+        printf("Ero fatal: Falha na alocação de memoria");
+        exit(1);
+    }
+
+    novo->valor = valor;
+    novo->anterior = anterior;
+    novo->proximo = proximo;
+    return novo;
+}
+
+
+int main() {
+    
+    return 0;
+}
